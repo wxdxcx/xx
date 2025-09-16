4f006b022055ca0e86f517e45bc59d90522bdc0a
david <david20250515@gmail.com>
2025-09-16 13:04:03 -0400

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -5,8 +5,8 @@ cat /dev/null > /var/log/secure
 cat /dev/null >  /var/log/wtmp
 
 
-sudo apt clean
-sudo apt autoremove -y 
+#sudo apt clean
+#sudo apt autoremove -y 
 
 
 sudo journalctl --vacuum-time=7d
