51a4b89d08a3ad04e885060d59f07713ca2c8635
david <david20250515@gmail.com>
2025-09-16 11:55:16 -0400

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-
-cat /dev/null > /var/log/wtmp
 cat /dev/null > /var/log/btmp
 cat /dev/null > /var/log/lastlog
 cat /dev/null > /var/log/secure
+cat /dev/null >  /var/log/wtmp
+
+
+sudo apt clean
+sudo apt autoremove -y 
+
+
+sudo journalctl --vacuum-time=7d
+
+
+sudo rm -rf /tmp/*
+sudo find /var/tmp -type f -mtime +7 -delete
 
 history -c && history -w
